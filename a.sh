ec2d585400e1d6e147810144dec8d1c986f8e207
TomoriNao <mengleshan23@mails.ucas.ac.cn>
2024-04-25 16:28:17 +0800

    origin

---
+++ a.sh
@@ -0,0 +1,18 @@
+datapath=/data06/yyq/mls/licom2_/addwater/exe/
+law=MMEAN
+
+fileslist=$(ls "$datapath$law"*)
+file_first=$(echo "$fileslist" | head -n 1)
+year_start=$((10#$(echo "$file_first" | grep -oP "${law}\d+" | sed "s/${law}//")))
+file_end=$(echo "$fileslist" | tail -n 1)
+year_end=$((10#$(echo "$file_end" | grep -oP "${law}\d+" | sed "s/${law}//")))
+number=$(($year_end - $year_start +1 ))
+
+for ((i=1; i<=number; i++)); do
+year=$(($year_start + $i - 1))
+input=$(ls $fileslist|grep $(printf "%04d" $year))
+cdo ensmean $input $datapath"YME"$(printf "%04d" $i).nc
+done
+
+cdo mergetime $(ls "$datapath""YME"*) $datapath"YME".nc
+rm -f $(ls $datapath"YME"????.nc)
